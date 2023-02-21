3ecef08c7e8d939fd559e9a92c52549deffa2e10
Fathur <fathur.alfarizy02@gmail.com>
2023-02-21 07:53:41 +0700

    first program

---
+++ tes.py
@@ -0,0 +1,9 @@
+from math import pi
+
+
+def circleArea(r):
+    '''
+    This function will calculate the area of the circle where r is the radius of the circle.
+    PI*r^2
+    '''
+    return pi*r ^ 2
